22544d9e0f4444987a5f65048d1cd707edfa6837
bluerune <bluerune9@gmail.com>
2023-03-24 00:21:24 -0300

    Start and pause menu
    Created StartScene, MainScene and pause menu

---
+++ ProjectBearWithUs/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public static bool isGamePaused = false;
+    [SerializeField] GameObject pauseMenu;
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit!");
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+        Debug.Log("Start Game");
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+        Debug.Log("Back to star menu");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePaused)
+            {
+                ResumeGame();
+            }
+            else 
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+        isGamePaused = false;
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+}
